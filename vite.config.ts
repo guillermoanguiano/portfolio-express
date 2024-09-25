a2292e9885679ce411a4f41447fd1f823ece615e
guillermoanguiano <jesus2001guillermo2@gmail.com>
2024-09-24 19:29:05 -0700

    build

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio-express/',
   plugins: [react(), ViteImageOptimizer({
     png: {
       quality: 80,
